04c1c77252beb25213d599d4c5cd5a6ef9e64d35
arthur-cabral <arthurmartins10052002@gmail.com>
2023-09-11 19:17:13 -0300

    docs: Adding Swagger in project

---
+++ Project/src/main/java/com/example/backestopenda/config/ConfigOpenApi.java
@@ -0,0 +1,23 @@
+package com.example.backestopenda.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigOpenApi {
+
+    @Bean
+    public OpenAPI customOpenApi(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("RESTFul API Estopenda")
+                        .version("v1")
+                        .description("RESTFul API of project 'Estopenda'")
+                        .license(
+                                new License()
+                                        .name("Apache 2.0")));
+    }
+}
